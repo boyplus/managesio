b2924b8642177a825af8e42b8198652c7ac61b95
boyplus <ict224bj@gmail.com>
2022-12-01 21:49:46 +0700

    clear: cached

--- src/Managesio.ClientSide/src/pages/Homes.tsx
+++ src/Managesio.ClientSide/src/pages/Homes.tsx
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { todoApi } from '../api'
+import { Todo } from '../api/generated';
 
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const fetchTodos = async () => {
+    const res = await todoApi.getTodos();
+    setTodos(res.data);
+  }
+
   useEffect(() => {
-    todoApi.getTodos().then(res => {
-      const todos = res.data;
-      console.log(todos);
-    })
+    fetchTodos();
   }, [])
+
   return (
     <div>
       <h1>Project Management App</h1>
+      {todos.map(todo => {
+        return <div>
+          <h3>{todo.title}</h3>
+        </div>
+      })}
     </div>
   );
 }
