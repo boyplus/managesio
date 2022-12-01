da2b3cb8071b5b44b05c2a33520b4987043fe4d1
boyplus <ict224bj@gmail.com>
2022-12-01 16:57:29 +0700

    clear: cached

--- src/TodoApp.WebApi/Program.cs
+++ src/TodoApp.WebApi/Program.cs
@@ -18,7 +18,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
