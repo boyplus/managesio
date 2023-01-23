b1bc088d0518d52a8673d15e95026660c487c9df
boyplus <ict224bj@gmail.com>
2023-01-24 01:29:52 +0700

    update: title and routes

--- src/Managesio.ClientSide/src/App.tsx
+++ src/Managesio.ClientSide/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Homes';
 import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const App: React.FC = () => {
   return (
@@ -9,8 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route index element={<Home />}></Route>
         <Route path="profile" element={<Profile />}></Route>
-        <Route path="signin" element={<Profile />}></Route>
-        <Route path="signup" element={<Profile />}></Route>
+        <Route path="signin" element={<Signin />}></Route>
+        <Route path="signup" element={<Signup />}></Route>
       </Routes>
     </div>
   );
